447f53deae6b43b3f374ed752ca34e4bcfbb1282
kelseyfaunt <59808596+kelseyfaunt@users.noreply.github.com>
2022-04-24 23:18:40 -0400

    maybe

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const args = require('minimist')(process.argv.slice(2))
 args["port"]
-const port = args.port || process.env.PORT || 5555
+const port = args.port || process.env.PORT || 5000
 // console.log(args)
 
 const help = (`
